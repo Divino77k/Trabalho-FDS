18fd4098f193a6421fbca6259e746d1e0254451d
Divino77k <merk.oliveira1303@gmail.com>
2025-06-29 00:17:35 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+function mostrarMensagem() {
+    const mensagem = document.getElementById("mensagem");
+    mensagem.innerHTML = "Saudações! Obrigado por visitar meu site. Vamos evoluir juntos.";
+}
